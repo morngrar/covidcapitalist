1162ed2a4ae4156a228bed2cd77232b51698eddc
Svein-Kåre Bjørnsen <sveinkare@gmail.com>
2020-08-23 14:50:18 +0200

    removed unused files

--- hello.py
+++
@@ -1,9 +0,0 @@
-def hello():
-    print("Hello, World.")
-
-def event_glob_test(event_stream):
-    """Demonstration of how to change a global var from differnet module
-    """
-    event_stream.add("YOLO")
-
-    
--- tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-productionsystem.add_factory(game_data, "mask factories")
-                    pygame.mixer.Channel(2).play(pygame.mixer.Sound(factoryshopsound))
